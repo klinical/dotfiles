6c7d46e41b7ae3e5485eef5185a0324ed0b1af99
zachary nickels <zach.n500@gmail.com>
2025-05-09 21:23:19 -0500

    fix install script

--- scripts/install_pkgs.sh
+++ scripts/install_pkgs.sh
@@ -8,9 +8,9 @@ cd ~/Downloads
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S $(~/$GIT_F/pkgs/yay)
+yay -S $(cat ~/$GIT_F/pkgs/yay)
 
 cd ~/Downloads
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source ~/.bash_profile
-cargo install $(~/$GIT_F/pkgs/cargo)
+cargo install $(cat ~/$GIT_F/pkgs/cargo)
